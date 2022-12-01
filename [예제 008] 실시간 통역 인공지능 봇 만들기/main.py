81a8e005eea3d1ebe3bf677e7167b46a52bc050a
needleworm <bhban@kakao.com>
2022-12-01 17:38:14 +0900

    .

--- [예제 008] 실시간 통역 인공지능 봇 만들기/main.py
+++ [예제 008] 실시간 통역 인공지능 봇 만들기/main.py
@@ -7,10 +7,12 @@ https://github.com/needleworm/python101
 import utils as U
 
 val = ""
-while val not in ['1', '2']:
+while True:
     val = input("모드를 선택하세요.\n\t1. 한국어 -> 영어\n\t2. 영어 -> 한국어\n")
-    if val not in ['1', '2']:
-        print("\n입력값이 잘못되었습니다. (1 또는 2 입력)")
+    if val in ['1', '2']:
+        break
+
+    print("\n입력값이 잘못되었습니다. (1 또는 2 입력)")
 
 if val == '1':
     while True:
